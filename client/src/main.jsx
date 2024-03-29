c7e31a3c7ce9fc544ff9ec2d70592421011ee290
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-03-28 21:40:31 -0400

    slight edits to the main.jsx in client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,22 +2,27 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
-import LandingPage from './pages/landingPage.jsx';
+import HomePage from './pages/homePage.jsx';
 import Calendar from './pages/calendar.jsx';
 import Login from './pages/login.jsx';
+import Signup from './pages/newUser.jsx';
 
 const router = createBrowserRouter([
     {
-        path: '/',
+        path: '/login',
         element: <App />,
         children: [
             {
                 index: true,
-                element: <LandingPage />
+                element: <Login />
             },
             {
-                path: '/login',
-                element: <Login />,
+                path: '/signup',
+                element: <Signup />,
+            },
+            {
+                path: '/homepage',
+                element: <HomePage />
             },
             {
                 path: '/calendar',
