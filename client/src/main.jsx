ffdc49cd34027b3c9476601a9346f22506c34fba
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-03-28 21:27:33 -0400

    worked on the main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,1 +1,32 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import App from './App.jsx';
+import LandingPage from './pages/landingPage.jsx';
+import Calendar from './pages/calendar.jsx';
+import Login from './pages/login.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <LandingPage />
+            },
+            {
+                path: '/login',
+                element: <Login />,
+            },
+            {
+                path: '/calendar',
+                element: <Calendar />
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router = {router} />
+);
