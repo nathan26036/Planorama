93fef8c23fbebdbc9a2d03718bc3bd329d0c3ec0
nathan26036 <nathandickinson04@gmail.com>
2024-04-04 18:58:28 -0400

    Added description to modal and added save button

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -24,7 +24,7 @@ function MyVerticallyCenteredModal(props) {
         <h4>Enter the name of your new event:</h4>
         <input className="input" id="eventTitle" type="text" placeholder="Event Name"></input>
         <h4>Enter a short description of the event:</h4>
-        <input className="input" id="eventTitle" type="text" placeholder="Description"></input>
+        <input className="input" id="eventDescription" type="text" placeholder="Description"></input>
         <h4>Enter the date and time of your event:</h4>
         <input className="input" id="eventDate" type="datetime-local" placeholder="Date"></input>
       </Modal.Body>
